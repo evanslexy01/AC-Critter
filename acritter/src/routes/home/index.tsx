2090bb9c096f5ca6065c3383bc03b86a3697c49e
evanslexy01 <evans.lexy@gmail.com>
2022-10-19 22:50:11 -0500

    push

--- acritter/src/routes/home/index.tsx
+++ acritter/src/routes/home/index.tsx
@@ -1,14 +1,20 @@
-import { h } from 'preact';
-import style from './style.css';
+import { h } from "preact";
+import style from "./style.css";
 
 const Home = () => (
-	<div class={style.home}>
-	  <button class="btn">
-      <button class="btn-muted"><img class="btn_fish" src="fishy.svg"></img></button>
-      <button class="btn-muted"><img class="btn_butt" src="butterfly.svg"></img></button>
-      <button class="btn-muted"><img class="btn_squid" src="squid.svg"></img></button>
+  <div class={style.home}>
+    <button class="btn">
+      <button class="btn-muted">
+        <img class="btn_fish" src="fishy.svg"></img>
+      </button>
+      <button class="btn-muted">
+        <img class="btn_butt" src="butterfly.svg"></img>
+      </button>
+      <button class="btn-muted">
+        <img class="btn_squid" src="squid.svg"></img>
+      </button>
     </button>
-	</div>
+  </div>
 );
 
 export default Home;
